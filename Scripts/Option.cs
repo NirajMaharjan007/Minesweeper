2469eab621523ec11f1cd9d800d93b6dc8f00227
NirajAsusROG <niraj.mhrjn770@gmail.com>
2026-06-03 22:47:35 +0545

    Minor Add and Major Updates
    - Added Option  TODO: FIXES and testing
    - TODO : Add Imp things and fix unknown BUGS

---
+++ Scripts/Option.cs
@@ -0,0 +1,42 @@
+using Godot;
+
+namespace Minesweeper.Scripts;
+
+public partial class Option : Control
+{
+    private VBoxContainer mainContainer;
+    private OptionButton optionButton;
+    private RichTextLabel bodyLabel;
+
+    private Misc.Definition definition = Misc.Definition.Instance;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        mainContainer = GetNode<VBoxContainer>("MainContainer");
+
+        optionButton = mainContainer
+            .GetNode<PanelContainer>("MainPanel")
+            .GetNode<HBoxContainer>("HBoxContainer")
+            .GetNode<OptionButton>("OptionButton");
+
+        bodyLabel = mainContainer
+            .GetNode<PanelContainer>("PanelContainer")
+            .GetNode<VBoxContainer>("VBoxContainer")
+            .GetNode<RichTextLabel>("Body");
+
+        Init();
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+
+    private void Init()
+    {
+        string body = "THIS IS THE BODY";
+        bodyLabel.Text = body;
+    }
+}
